51ca4aec3fc2a6e15ff95ed233623f313085668a
Jacob <jacob.chiu168@hotmail.com>
2023-12-30 22:34:14 -0500

    add end con, done game

--- main.py
+++ main.py
@@ -12,7 +12,15 @@ if __name__ == "__main__":
     boardChoice = [
         "a", "b", "c", "d", "e", "f"
     ]
-    msgs = {0: "\033[1mPlaying...\033[0m", -1: "\033[1mInvalid Tnput!\033[0m", -2: "\033[1mInvalid Choice!\033[0m", -3: "\033[1mGo Again!\033[0m"}
+    msgs = {
+        0: "\033[1mPlaying...\033[0m", 
+        -1: "\033[1mInvalid Tnput!\033[0m", 
+        -2: "\033[1mInvalid Choice!\033[0m", 
+        -3: "\033[1mGo Again!\033[0m", 
+        -4: "\033[1mGame Over!\033[0m",
+        -5: "\033[1mPlayer One Won!\033[0m",
+        -6: "\033[1mPlayer Two Won!\033[0m",
+        -7: "\033[1mPlayer One and Two Tied!\033[0m"}
     msgCode = 0
 
     while gameOn:
@@ -32,7 +40,7 @@ if __name__ == "__main__":
         else:
             playerTurn = "Player Two's"
 
-        #os.system("cls")
+        os.system("cls")
         print(f"( \033[1m'q' to QUIT\033[0m )    ( \033[1m{playerTurn} Turn\033[0m )    ( {msgs[msgCode]} )")
         print()
 
@@ -58,8 +66,10 @@ if __name__ == "__main__":
         else:
             print()
 
-        # print(boardChoice)
-        # print(boardAmount)
+        int_boardAmount = [int(x) for x in boardAmount]
+        if sum(int_boardAmount[0:6]) == 0 or sum(int_boardAmount[7:13]) == 0:
+            msgCode = -4
+            break
 
         userChoice = str(input(f"\033[1mChoice: \033[0m"))
         
@@ -67,6 +77,8 @@ if __name__ == "__main__":
         if userChoice == 'q':
             gameOn = False
         elif userChoice in boardChoice:
+
+
             msgCode = 0
             if playerOne:
                 if userChoice == 'a':
@@ -98,14 +110,13 @@ if __name__ == "__main__":
             msgCode = -1
             continue
 
-        if userChoice in boardChoice and int(boardAmount[chosenBin]) == 0:
+        if boardAmount[chosenBin] == ' 0':
             msgCode = -2
+            continue
 
         for i in range (int(boardAmount[chosenBin])):
             real_index = (chosenBin + 1 + i) % 14
             boardAmount[real_index] = int(boardAmount[real_index])
-            #print(boardAmount[(chosenBin + 1 + i) % 14], (chosenBin + 1 + i) % 14, playerOne)
-
             #Standard logic (skip other players bin and add)
             if playerOne and real_index == 13:
                 continue
@@ -123,9 +134,8 @@ if __name__ == "__main__":
                 playerOne = not playerOne
 
             #last stone lands in empty (take opponets stones and put into bin)
-            if i == int(boardAmount[chosenBin]) - 1 and boardAmount[real_index] == 1 and real_index != 6 and real_index != 13:
-                print(boardAmount[(6 - real_index) + 6 - 1], (6 - real_index) + 6 - 1, real_index)
-                if playerOne:
+            if i == int(boardAmount[chosenBin]) - 1 and boardAmount[real_index] == 1 and real_index != 6 and real_index != 13 and boardAmount[(12 - real_index)] != ' 0' :
+                if playerOne and real_index < 6:
                     boardAmount[6] = int(boardAmount[6])
                     boardAmount[6] += 1
                     boardAmount[real_index] = int(boardAmount[real_index])
@@ -135,7 +145,7 @@ if __name__ == "__main__":
                     boardAmount[6] += boardAmount[(12 - real_index)]
                     boardAmount[(12 - real_index)] = 0
 
-                elif not playerOne: 
+                elif not playerOne and real_index > 6: 
                     boardAmount[13] = int(boardAmount[13])
                     boardAmount[13] += 1
                     boardAmount[real_index] = int(boardAmount[real_index])
@@ -144,7 +154,32 @@ if __name__ == "__main__":
                     boardAmount[(12 - real_index)] = int(boardAmount[12 - real_index])
                     boardAmount[13] += boardAmount[(12 - real_index)]
                     boardAmount[(12 - real_index)] = 0
-
-
+        
         boardAmount[chosenBin] = 0
         playerOne = not playerOne
+
+    for i, ele in enumerate(boardAmount):
+        ele = int(ele)
+        if ele < 10:
+            boardAmount[i] = " " + str(ele)
+        else: 
+            ele = str(ele)
+
+    os.system("cls")
+    print(f"( \033[1m'q' to QUIT\033[0m )    ( \033[1m{playerTurn} Turn\033[0m )    ( {msgs[msgCode]} )")
+    print()
+    print()
+
+    print(f"{padding}+----+----+----+----+----+----+----+----+")
+    print(f"{padding}|    | {boardAmount[12]} | {boardAmount[11]} | {boardAmount[10]} | {boardAmount[9]} | {boardAmount[8]} | {boardAmount[7]} |    |")
+    print(f"{padding}| {boardAmount[13]} +----+----+----+----+----+----+ {boardAmount[6]} |")
+    print(f"{padding}|    | {boardAmount[0]} | {boardAmount[1]} | {boardAmount[2]} | {boardAmount[3]} | {boardAmount[4]} | {boardAmount[5]} |    |")
+    print(f"{padding}+----+----+----+----+----+----+----+----+")
+
+    print()
+    if int_boardAmount[6] > int_boardAmount[13]:
+        print(f"( {msgs[-5]} )")
+    elif int_boardAmount[6] < int_boardAmount[13]:
+        print(f"( {msgs[-6]} )")
+    else:
+        print(f"( {msgs[-7]} )")
